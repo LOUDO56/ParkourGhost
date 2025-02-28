e4ad489c5d2fde7139fa8f59dbeff897d144d6e7
LOUDO <lucaslelouer@gmail.com>
2025-02-28 18:02:26 +0100

    Update: Get name of recorded parkour in json file instead of database

--- src/main/java/fr/loudo/parkourGhost/commands/ParkourGhostTabCompleter.java
+++ src/main/java/fr/loudo/parkourGhost/commands/ParkourGhostTabCompleter.java
@@ -1,5 +1,7 @@
 package fr.loudo.parkourGhost.commands;
 
+import fr.loudo.parkourGhost.manager.PlayersDataManager;
+import fr.loudo.parkourGhost.recordings.RecordingData;
 import io.github.a5h73y.parkour.Parkour;
 import io.github.a5h73y.parkour.commands.ParkourCommands;
 import io.github.a5h73y.parkour.database.DatabaseManager;
@@ -9,11 +11,13 @@ import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import pro.husk.Database;
 
+import java.io.IOException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class ParkourGhostTabCompleter implements TabCompleter {
@@ -23,7 +27,7 @@ public class ParkourGhostTabCompleter implements TabCompleter {
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
         List<String> completions = new ArrayList<>();
-        List<String> coursesNameCompleted = getCompletedCourse((Player) sender);
+        List<String> coursesNameCompleted = getRecordedCourses((Player) sender);
         if (args.length == 1) {
             completions.addAll(ARG1_OPTIONS);
             if (sender.hasPermission("parkourghost.admin")) {
@@ -38,23 +42,13 @@ public class ParkourGhostTabCompleter implements TabCompleter {
         return completions;
     }
 
-    private List<String> getCompletedCourse(Player p) {
-        DatabaseManager databaseManager = Parkour.getInstance().getDatabaseManager();
-        Database database = Parkour.getInstance().getDatabaseManager().getDatabase();
-        List<String> result = new ArrayList<>();
-
-        try (PreparedStatement statement = database.getConnection().prepareStatement("SELECT name FROM course INNER JOIN time ON course.courseId = time.courseId WHERE playerId = ?")) {
-            statement.setString(1, databaseManager.getPlayerId(p));
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                result.add(resultSet.getString("name"));
-            }
-
-            resultSet.getStatement().close();
-        } catch (SQLException e) {
-            throw new RuntimeException("Couldn't get player completed courses: " + e);
+    private List<String> getRecordedCourses(Player p) {
+        try {
+            HashMap<String, RecordingData> recordingData = PlayersDataManager.getRecordingData(p).getRecordedRuns();
+            return new ArrayList<>(recordingData.keySet());
+        } catch (IOException e) {
+            throw new RuntimeException("Couldn't get recorded courses of " + p.getName() + ": " + e);
         }
 
-        return result;
     }
 }
