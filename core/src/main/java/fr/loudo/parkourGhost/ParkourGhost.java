7376484e7f75903b2051d872b680d8cdd8f6ffb4
LOUDO <lucaslelouer@gmail.com>
2025-03-12 21:39:58 +0100

    Fix: 1.20.6 bad nms version

--- core/src/main/java/fr/loudo/parkourGhost/ParkourGhost.java
+++ core/src/main/java/fr/loudo/parkourGhost/ParkourGhost.java
@@ -66,7 +66,7 @@ public final class ParkourGhost extends JavaPlugin {
                 version = "1_19_R3";
                 break;
             case "1.20.6":
-                version = "1_18_R4";
+                version = "1_20_R4";
                 break;
             case "1.21.4":
                 version = "1_21_R3";
